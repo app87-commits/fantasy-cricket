25ac7cb2ef3496cd901b5238aeaaaeb7fef5002f
Nikhil Sangam (Consultant) <NSangam@mdot.state.md.us>
2026-02-06 11:22:50 -0500

    security update1

--- firebase.js
+++ firebase.js
@@ -6,3 +6,26 @@ admin.initializeApp({
 
 const db = admin.firestore();
 module.exports = db;
+
+/* Copied from Firebase sdk
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPrmDt5lF3k8ztabxakQ7IBHaFaBLb4UA",
+  authDomain: "fantasy-cricket-group.firebaseapp.com",
+  projectId: "fantasy-cricket-group",
+  storageBucket: "fantasy-cricket-group.firebasestorage.app",
+  messagingSenderId: "877806220984",
+  appId: "1:877806220984:web:76b463dbc231977590664d",
+  measurementId: "G-YNXG2L07KP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); */
